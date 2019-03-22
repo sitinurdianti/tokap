f5733c778852f6265c01075e5bb919db91122484
sitinurdianti <sitinurdiantiiiii@gmail.com>
2019-03-22 20:10:27 +0700

    eps-22

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -14,4 +14,13 @@ class KategoriController extends Controller
 
     	return view('admin.pages.kategori.daftar',['data'=>$data]);
     }
+    public function add()
+    {
+    	return view('admin.pages.kategori.add');
+    }
+
+    public function save(Request $req)
+    {
+    	return 'Fungsi Save';
+    }
 }
